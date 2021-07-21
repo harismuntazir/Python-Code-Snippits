0027ccea1d6939e5be287c0d44d60e1f850b2187
hm <haris.0813.m@gmail.com>
2021-07-21 11:55:10 +0530

    Basics Of Python
    some basic code snippits, very handy at times

---
+++ Python Basics.py
@@ -0,0 +1,29 @@
+#concatinate
+"some" + "text"
+str(1) + " is an Integer"
+
+#convert to string
+str(some non string data)
+
+#length of string
+len("a string")
+data = "a string"
+data.__len__
+
+#read text from network request response 
+response.text
+#size of the request response
+len(response.content)
+
+#take user input
+data = input("Enter What you Want: ")
+
+#find and replace from text string
+astring = "this text"
+replaced = astring.replace("this", "with that")
+#output will be: 
+# ........with that text...........
+
+# join two path into one
+from os import path
+os.path.join(basePath, cznId.replace("/", ""))
