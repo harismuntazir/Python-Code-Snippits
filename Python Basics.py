93fb1bc0f7be343c82ba0a69f4d36c41e6b91823
hm <haris.0813.m@gmail.com>
2021-07-21 12:23:28 +0530

    Update Python Basics.py

--- Python Basics.py
+++ Python Basics.py
@@ -27,3 +27,33 @@ replaced = astring.replace("this", "with that")
 # join two path into one
 from os import path
 os.path.join(basePath, cznId.replace("/", ""))
+
+
+#find number of files in a directory
+import os
+path, dirs, files = next(os.walk("Citizens"))
+file_count = len(files)
+print(file_count)
+
+#use threading, its something like coroutines in kotlin
+def myFunc(arg):
+  print(arg)
+  
+from threading import Thread
+Thread(target=myFunc, args=(1, )).start()
+Thread(target=myFunc, args=(2, )).start()
+
+#pause or delay working process like this
+import time
+for x in range(1,5):
+  print(x)
+  time.sleep(1)   #where 1 is a one second (time is in seconds)
+  
+  
+  
+#Make a directory if its does not exists
+from os import makedirs
+def makeFilePath (dirName):
+    if not os.path.isdir(dirName):
+        os.mkdir(dirName) #make directory if it doesn't exist
+        print("Created Directory " + dirName)
